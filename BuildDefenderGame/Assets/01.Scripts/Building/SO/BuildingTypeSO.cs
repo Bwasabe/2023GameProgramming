1f3f9dca42963635aeb4af6eec15bb70a41f349a
Bwasabe <jixg20@gmail.com>
2023-03-02 15:42:19 +0900

    Initialize

---
+++ BuildDefenderGame/Assets/01.Scripts/Building/SO/BuildingTypeSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( menuName = "ScriptableObject/BuildingType", fileName = "BuildingType")]
+public class BuildingTypeSO : ScriptableObject
+{
+    public KeyCode keycode = KeyCode.A;
+    public int index;
+    public string nameString;
+    public Transform prefab;
+
+}
