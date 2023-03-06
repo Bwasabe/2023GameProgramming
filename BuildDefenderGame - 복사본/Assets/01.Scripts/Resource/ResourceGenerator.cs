debccd565d0a058cb518a91eb5393fa4ee3491b3
Bwasabe <jixg20@gmail.com>
2023-03-06 14:52:22 +0900

    프로젝트 복사

---
+++ BuildDefenderGame - 복사본/Assets/01.Scripts/Resource/ResourceGenerator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// [RequireComponent(typeof(Holder))]
+public class ResourceGenerator : MonoBehaviour
+{
+    private BuildingTypeSO _buildingType;
+    private float _timer;
+    private float _timerMax;
+    
+    private void Awake() {
+        _buildingType = GetComponent<BuildingTypeHolder>().BuildingType;
+        _timerMax = _buildingType.resourceGeneratorData.timerMax;
+        
+    }
+
+    private void Update() {
+        _timer -= Time.deltaTime;
+        if(_timer <= 0)
+        {
+            _timer += _timerMax;
+            ResourceManager.Instance.AddResource(_buildingType.resourceGeneratorData.resourceType, 1);
+            //Debug.Log(_buildingType.resourceGeneratorData.resourceType.nameString);
+        }
+    }
+}
---
+++ BuildDefenderGame - 복사본/Assets/01.Scripts/Resource/ResourceManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public static ResourceManager Instance{ get; private set;}
+
+    private Dictionary<ResourceTypeSO, int> _resourceAmountDictionary;
+
+    private void Awake()
+    {
+        Instance = this;
+        _resourceAmountDictionary = new();
+
+        ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(nameof(ResourceTypeListSO));
+
+        foreach (ResourceTypeSO resourceType in resourceTypeList.list)
+        {
+            _resourceAmountDictionary[resourceType] = 0;
+        }
+        TestLogResourceAmountDictionary();
+    }
+
+    private void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.Q))
+        // {
+        //     ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(nameof(ResourceTypeListSO));
+        //     AddResource(resourceTypeList.list[0], 1);
+        // }
+
+        // if (Input.GetKeyDown(KeyCode.W))
+        // {
+        //     ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(nameof(ResourceTypeListSO));
+        //     AddResource(resourceTypeList.list[1], 1);
+        // }
+
+        // if (Input.GetKeyDown(KeyCode.E))
+        // {
+        //     ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(nameof(ResourceTypeListSO));
+        //     AddResource(resourceTypeList.list[2], 1);
+        // }
+
+        // if(Input.GetKeyDown(KeyCode.R))
+        // {
+        //     ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(nameof(ResourceTypeListSO));
+        //     AddResource(resourceTypeList.list[3], 1);
+        // }
+    }
+
+    private void TestLogResourceAmountDictionary()
+    {
+        foreach (ResourceTypeSO resourceType in _resourceAmountDictionary.Keys)
+        {
+            Debug.Log(resourceType.nameString + " : " + _resourceAmountDictionary[resourceType]);
+        }
+    }
+
+    public void AddResource(ResourceTypeSO resourceType, int amount)
+    {
+        _resourceAmountDictionary[resourceType] += amount;
+        TestLogResourceAmountDictionary();
+
+    }
+}
