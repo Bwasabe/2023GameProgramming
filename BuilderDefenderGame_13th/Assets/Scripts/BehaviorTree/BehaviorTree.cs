4bb85da666622ec0b022819ea4f379b44f16394a
Bwasabe <jixg20@gmail.com>
2023-04-20 14:03:13 +0900

    폴더 변경

---
+++ BuilderDefenderGame_13th/Assets/Scripts/BehaviorTree/BehaviorTree.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class BehaviourTree : MonoBehaviour, IUpdateAble, IGetComponentAble
+{
+    public abstract BT_Variable Variable{ get; set; }
+
+    protected BT_Node _root;
+
+    public bool IsStop{ get; set; } = false;
+    
+
+    protected virtual void Start()
+    {
+        _root = SetupTree();
+    }
+
+    protected virtual void OnEnable()
+    {
+        //UpdateManager.Instance.RegisterObject(this);
+    }
+
+    protected void OnDisable()
+    {
+        //UpdateManager.Instance?.UnRegisterObject(this);
+    }
+
+    public void FlipLeft()
+    {
+        Vector3 scale = transform.localScale;
+        float scaleX = Mathf.Abs(scale.x);
+        
+        scale.x = scaleX * -1f;
+        
+        transform.localScale = scale;
+    }
+
+    public void FlipRight()
+    {
+        Vector3 scale = transform.localScale;
+        float scaleX = Mathf.Abs(scale.x);
+        
+        scale.x = scaleX;
+        
+        transform.localScale = scale;
+    }
+    
+
+    protected abstract BT_Node SetupTree();
+    public void OnUpdate()
+    {
+        if(!IsStop)
+        {
+            _root?.Execute();
+        }
+    }
+
+}
